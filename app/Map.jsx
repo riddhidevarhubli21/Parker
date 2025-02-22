ddd1bdaf7da13fc0d17d267ff991ab8e7afc1e1d
Alice Yang <alicyangs@gmail.com>
2025-02-22 11:50:30 -0600

    adding base files

---
+++ app/Map.jsx
@@ -0,0 +1,13 @@
+import MapViewDirections from 'react-native-maps-directions';
+import MapView, { Marker, Geojson as MapsGeojson } from 'react-native-maps';
+import Geojson from 'react-native-geojson';
+import { View } from 'react-native';
+
+export default function Map() {
+  return (
+    <View>
+      <MapView>
+      </MapView>
+    </View>
+  )
+}
---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'; /* these are react features that are used, to store and update data we use useState and when something changes useEffect*/
+import { Text, View, TextInput, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import MapView, {Polyline, PROVIDER_GOOGLE} from "react-native-maps";
+
+export default function ParkingScreen() {
+  return (
+    <View style = {StyleSheet.cotainer}>
+      {/* App Title*/}
+      
+    </View>
+  );
+}
